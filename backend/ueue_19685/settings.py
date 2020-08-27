d5fe07775b23562475040765a1fe7e7564e86491
Crowdbotics <team@crowdbotics.com>
2020-08-27 22:30:28 +0000

    Update models

--- backend/ueue_19685/settings.py
+++ backend/ueue_19685/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     "location",
     "wallet",
     "task_category",
+    "chat",
+    "chat_user_profile",
 ]
 LOCAL_APPS = [
     "home",
--- backend/users/models.py
+++ backend/users/models.py
@@ -7,9 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
-    email = models.EmailField(null=True, blank=True, max_length=255,)
     first_name = models.CharField(null=True, blank=True, max_length=255,)
     last_name = models.CharField(null=True, blank=True, max_length=255,)
+    email = models.EmailField(null=True, blank=True, max_length=255,)
     timestamp_created = models.DateTimeField(null=True, blank=True, auto_now_add=True,)
     last_updated = models.DateTimeField(null=True, blank=True, auto_now=True,)
 
